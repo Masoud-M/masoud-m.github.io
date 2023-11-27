0268b582dbf929c3e26a7e5145dd1a82197d95f6
Masoud-M <48066903+Masoud-M@users.noreply.github.com>
2023-11-27 11:47:46 +0330

    just trying to test something

---
+++ index.js
@@ -0,0 +1,9 @@
+function closeSidebar() {
+  document.getElementById("sidebar-container").style.display = "none";
+  document.getElementById("open-sidebar").style.display = "unset";
+}
+
+function openSidebar() {
+  document.getElementById("sidebar-container").style.display = "unset";
+  document.getElementById("open-sidebar").style.display = "none";
+}
